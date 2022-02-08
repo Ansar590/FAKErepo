9d356b0f70279b8f99014a37c01672011026b967
illuzX <98640218+illuzX@users.noreply.github.com>
2022-02-08 12:40:02 +0530

    Fixes

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -418,7 +418,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
-        
+       )
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.HELP_TXT.format(query.from_user.mention),
